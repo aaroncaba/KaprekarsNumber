f39933da848f6f651217034580848708c2724294
Aaron Caba <aaroncaba@yahoo.com>
2024-09-01 11:36:55 -0400

    Refactor class to use stored num_of_digits 'n'

--- KaprekarsConstant.py
+++ KaprekarsConstant.py
@@ -7,10 +7,10 @@ import cProfile
 
 class KaprekaersConstant():
     def __init__(self, n):
-        self.num_of_digits = n
+        self.n = n
         self.zeros = np.zeros(n, dtype=np.int32)
 
-    def digits_of(self, k: int, num_of_digits: int, type: str):
+    def digits_of(self, k: int,  type: str):
         # get individual digits
         digits = self.zeros.copy()
         s = str(k)
@@ -34,7 +34,7 @@ class KaprekaersConstant():
             m = m*10
         return ret
 
-    def iter_count(self, k: int, num_of_digits: int):
+    def iter_count(self, k: int ):
         knew = k
         kold = 0
         count = 0
@@ -43,9 +43,9 @@ class KaprekaersConstant():
         while kold != knew or count == 0:
             kold = knew
             nsmall = self.digits_to_num(
-                self.digits_of(knew, num_of_digits, type='small'))
+                self.digits_of(knew,  type='small'))
             nbig = self.digits_to_num(self.digits_of(
-                knew, num_of_digits, type='big'))
+                knew,  type='big'))
             knew = nbig - nsmall
             count += 1
             # print(count, kold)
@@ -56,14 +56,14 @@ class KaprekaersConstant():
 
         return count-1, knew
 
-    def calc_it(self, n: int, max_n=None) -> None:
+    def calc_it(self,  max_n=None) -> None:
         if max_n is None:
-            max_n = 10**n-1
-        max_n = min(max_n, (10**n)-1)
+            max_n = 10**self.n-1
+        max_n = min(max_n, (10**self.n)-1)
         count = np.zeros(max_n+1, dtype=np.int32)
         final = np.zeros(max_n+1, dtype=np.int32)
         for i in range(1, max_n+1):
-            cc, ff = self.iter_count(i, n)
+            cc, ff = self.iter_count(i)
             count[i] = cc
             final[i] = ff
             if i / 1000 == math.floor(i/1000):
@@ -73,15 +73,15 @@ class KaprekaersConstant():
 
 if __name__ == "__main__":
 
-    n = 6
+    num_of_digits = 6
     profile = True
 
-    kc = KaprekaersConstant(n)
+    kc = KaprekaersConstant(num_of_digits)
 
     if profile:
-        cProfile.run('kc.calc_it(n,100000)', sort='tottime')
+        cProfile.run('kc.calc_it(100000)', sort='tottime')
     else:
-        count, final = kc.calc_it(n, 10**5)
+        count, final = kc.calc_it(10**5)
         plt.figure(0)
         plt.plot(final)
         plt.figure(1)
--- KaprekarsConstant_test.py
+++ KaprekarsConstant_test.py
@@ -7,26 +7,26 @@ class TestTheThing(unittest.TestCase):
     def test_digits_of_returns_list_of_sorted_digits(self):
         kc = KaprekaersConstant(4)
         self.assertTrue(np.array_equal(
-            [1, 2, 3, 4], kc.digits_of(1324, 4, type='small')))
+            [1, 2, 3, 4], kc.digits_of(1324,  type='small')))
 
     def test_digits_of_returns_list_of_reverse_sorted_digits(self):
         kc = KaprekaersConstant(4)
         self.assertTrue(np.array_equal(
-            [4, 3, 2, 1], kc.digits_of(1324, 4, type='big')))
+            [4, 3, 2, 1], kc.digits_of(1324,  type='big')))
 
     def test_digits_of_reutrns_list_of_length_four(self):
         kc = KaprekaersConstant(4)
-        self.assertEqual(4, len(kc.digits_of(123, 4, type='small')))
+        self.assertEqual(4, len(kc.digits_of(123,  type='small')))
 
     def test_digits_of_returns_list_of_zeros_for_zero_input(self):
         kc = KaprekaersConstant(4)
         self.assertTrue(np.array_equal(
-            [0, 0, 0, 0], kc.digits_of(0, 4, type='small')))
+            [0, 0, 0, 0], kc.digits_of(0,  type='small')))
 
     def test_digits_of_returns_5_for_5_digit_number(self):
         kc = KaprekaersConstant(5)
         self.assertTrue(np.array_equal(
-            [1, 2, 3, 4, 5], kc.digits_of(12345, 5, type='small')))
+            [1, 2, 3, 4, 5], kc.digits_of(12345, type='small')))
 
     def test_digits_to_num_creates_correct_int(self):
         kc = KaprekaersConstant(4)
@@ -34,15 +34,15 @@ class TestTheThing(unittest.TestCase):
 
     def test_iter_count_is_five_for_1001(self):
         kc = KaprekaersConstant(4)
-        count, final = kc.iter_count(1001, 4)
+        count, final = kc.iter_count(1001)
         self.assertEqual(4, count)
 
     def test_iter_count_is_1_for_6174(self):
         kc = KaprekaersConstant(4)
-        count, final = kc.iter_count(6174, 4)
+        count, final = kc.iter_count(6174)
         self.assertEqual(0, count)
 
     def test_iter_count_is_1_for_11111(self):
         kc = KaprekaersConstant(5)
-        count, final = kc.iter_count(11111, 5)
+        count, final = kc.iter_count(11111)
         self.assertEqual(1, count)
