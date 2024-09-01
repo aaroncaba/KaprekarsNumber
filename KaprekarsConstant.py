e592e41de2f2286a712fea038ec229eb4e02b6c4
Aaron Caba <aaroncaba@yahoo.com>
2024-09-01 11:27:22 -0400

    Put it in a class

--- KaprekarsConstant.py
+++ KaprekarsConstant.py
@@ -5,81 +5,83 @@ import numpy.typing as npt
 import cProfile
 
 
-def digits_of(k: int, num_of_digits: int):
-    ret = np.zeros(num_of_digits, dtype=np.int32)
-    s = str(k)
-    for i in range(len(s)):
-        ret[i] = int(s[i])
+class KaprekaersConstant():
+    def __init__(self, n):
+        self.num_of_digits = n
+        self.zeros = np.zeros(n, dtype=np.int32)
 
-    return ret
+    def digits_of(self, k: int, num_of_digits: int, type: str):
+        # get individual digits
+        digits = self.zeros.copy()
+        s = str(k)
+        for i in range(len(s)):
+            digits[i] = int(s[i])
 
+        if type == 'big':
+            digits[::-1].sort()
+        elif type == 'small':
+            digits.sort()
+        else:
+            raise Exception(f'Badd Argument:  {type}')
 
-def digits_bigf(k: int, num_of_digits: int):
-    dd = digits_of(k, num_of_digits)
-    dd[::-1].sort()
-    return dd
+        return digits
 
+    def digits_to_num(self, digits: npt.NDArray):
+        m = 1
+        ret = 0
+        for dd in digits[::-1]:
+            ret = ret + m*dd
+            m = m*10
+        return ret
 
-def digits_smallf(k: int, num_of_digits):
-    dd = digits_of(k, num_of_digits)
-    dd.sort()
-    return dd
+    def iter_count(self, k: int, num_of_digits: int):
+        knew = k
+        kold = 0
+        count = 0
+        klist = [k]
 
+        while kold != knew or count == 0:
+            kold = knew
+            nsmall = self.digits_to_num(
+                self.digits_of(knew, num_of_digits, type='small'))
+            nbig = self.digits_to_num(self.digits_of(
+                knew, num_of_digits, type='big'))
+            knew = nbig - nsmall
+            count += 1
+            # print(count, kold)
 
-def digits_to_num(digits: npt.NDArray):
-    m = 1
-    ret = 0
-    for dd in digits[::-1]:
-        ret = ret + m*dd
-        m = m*10
-    return ret
+            if knew in klist:
+                break
+            klist.append(knew)
 
+        return count-1, knew
 
-def iter_count(k: int, num_of_digits: int):
-    knew = k
-    kold = 0
-    count = 0
-    klist = [k]
-
-    while kold != knew or count == 0:
-        kold = knew
-        nsmall = digits_to_num(digits_smallf(knew, num_of_digits))
-        nbig = digits_to_num(digits_bigf(knew, num_of_digits))
-        knew = nbig - nsmall
-        count += 1
-        # print(count, kold)
-
-        if knew in klist:
-            break
-        klist.append(knew)
-
-    return count-1, knew
-
-
-def calc_it(n: int, max_n=None) -> None:
-    if max_n is None:
-        max_n = 10**n-1
-    max_n = min(max_n, (10**n)-1)
-    count = np.zeros(max_n+1, dtype=np.int32)
-    final = np.zeros(max_n+1, dtype=np.int32)
-    for i in range(1, max_n+1):
-        cc, ff = iter_count(i, n)
-        count[i] = cc
-        final[i] = ff
-        if i / 1000 == math.floor(i/1000):
-            print(i)
-    return count, final
+    def calc_it(self, n: int, max_n=None) -> None:
+        if max_n is None:
+            max_n = 10**n-1
+        max_n = min(max_n, (10**n)-1)
+        count = np.zeros(max_n+1, dtype=np.int32)
+        final = np.zeros(max_n+1, dtype=np.int32)
+        for i in range(1, max_n+1):
+            cc, ff = self.iter_count(i, n)
+            count[i] = cc
+            final[i] = ff
+            if i / 1000 == math.floor(i/1000):
+                print(i)
+        return count, final
 
 
 if __name__ == "__main__":
 
     n = 6
-
     profile = True
+
+    kc = KaprekaersConstant(n)
+
     if profile:
-        cProfile.run('calc_it(n,100000)', sort='tottime')
+        cProfile.run('kc.calc_it(n,100000)', sort='tottime')
     else:
-        count, final = calc_it(n, 10**5)
+        count, final = kc.calc_it(n, 10**5)
         plt.figure(0)
         plt.plot(final)
         plt.figure(1)
--- KaprekarsConstant_test.py
+++ KaprekarsConstant_test.py
@@ -3,36 +3,46 @@ from KaprekarsConstant import *
 
 
 class TestTheThing(unittest.TestCase):
-    def test_digits_of_returns_list_of_digits(self):
-        self.assertTrue(np.array_equal([1, 3, 2, 4], digits_of(1324, 4)))
+
+    def test_digits_of_returns_list_of_sorted_digits(self):
+        kc = KaprekaersConstant(4)
+        self.assertTrue(np.array_equal(
+            [1, 2, 3, 4], kc.digits_of(1324, 4, type='small')))
+
+    def test_digits_of_returns_list_of_reverse_sorted_digits(self):
+        kc = KaprekaersConstant(4)
+        self.assertTrue(np.array_equal(
+            [4, 3, 2, 1], kc.digits_of(1324, 4, type='big')))
 
     def test_digits_of_reutrns_list_of_length_four(self):
-        self.assertEqual(4, len(digits_of(123, 4)))
+        kc = KaprekaersConstant(4)
+        self.assertEqual(4, len(kc.digits_of(123, 4, type='small')))
 
     def test_digits_of_returns_list_of_zeros_for_zero_input(self):
-        self.assertTrue(np.array_equal([0, 0, 0, 0], digits_of(0, 4)))
+        kc = KaprekaersConstant(4)
+        self.assertTrue(np.array_equal(
+            [0, 0, 0, 0], kc.digits_of(0, 4, type='small')))
 
     def test_digits_of_returns_5_for_5_digit_number(self):
-        self.assertTrue(np.array_equal([1, 2, 3, 4, 5], digits_of(12345, 5)))
+        kc = KaprekaersConstant(5)
+        self.assertTrue(np.array_equal(
+            [1, 2, 3, 4, 5], kc.digits_of(12345, 5, type='small')))
 
     def test_digits_to_num_creates_correct_int(self):
-        self.assertEqual(1324, digits_to_num([1, 3, 2, 4]))
-
-    def test_digits_bigf(self):
-        self.assertTrue(np.array_equal([6, 3, 2, 1], digits_bigf(3216, 4)))
-
-    def test_digits_smallf(self):
-        self.assertTrue(np.array_equal([1, 2, 3, 6], digits_smallf(3216, 4)))
+        kc = KaprekaersConstant(4)
+        self.assertEqual(1324, kc.digits_to_num([1, 3, 2, 4]))
 
     def test_iter_count_is_five_for_1001(self):
-        count, final = iter_count(1001, 4)
+        kc = KaprekaersConstant(4)
+        count, final = kc.iter_count(1001, 4)
         self.assertEqual(4, count)
 
     def test_iter_count_is_1_for_6174(self):
-        count, final = iter_count(6174, 4)
+        kc = KaprekaersConstant(4)
+        count, final = kc.iter_count(6174, 4)
         self.assertEqual(0, count)
 
     def test_iter_count_is_1_for_11111(self):
-        count, final = iter_count(11111, 5)
+        kc = KaprekaersConstant(5)
+        count, final = kc.iter_count(11111, 5)
         self.assertEqual(1, count)
-
